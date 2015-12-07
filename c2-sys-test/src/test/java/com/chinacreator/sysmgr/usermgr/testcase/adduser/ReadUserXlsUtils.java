57f7bd53a1a334558baa90e6f3b13a9271b4fcb9
xin.cao <xin.cao@chinacreator.com>
2015-12-07 17:01:14 +0800

    测试文件调整

--- c2-sys-test/src/test/java/com/chinacreator/sysmgr/usermgr/testcase/adduser/ReadUserXlsUtils.java
+++ c2-sys-test/src/test/java/com/chinacreator/sysmgr/usermgr/testcase/adduser/ReadUserXlsUtils.java
@@ -25,8 +25,8 @@ public class ReadUserXlsUtils  {
 
 		private final String xlspath = Common.getResdataPath()+"/UserData.xls";
 		private static ReadUserXlsUtils instance;
-		public static final int NORMALDATA = 0;       	//Õı³£Á÷Êı¾İ´ÓµÚÒ»¸ösheetÖĞ¶ÁÈ¡
-		public static final int EXCEPTIONDATA = 1;		//Òì³£Á÷Êı¾İ´ÓµÚ¶ş¸ösheetÖĞ¶ÁÈ¡
+		public static final int NORMALDATA = 0;       	//æ­£å¸¸æµæ•°æ®ä»ç¬¬ä¸€ä¸ªsheetä¸­è¯»å–
+		public static final int EXCEPTIONDATA = 1;		//å¼‚å¸¸æµæ•°æ®ä»ç¬¬äºŒä¸ªsheetä¸­è¯»å–
 		
 		public static ReadUserXlsUtils getInstance(){
 			if(instance==null){
@@ -55,7 +55,7 @@ public class ReadUserXlsUtils  {
 			}
 
 			List<UserBean> list = new ArrayList<UserBean>();
-			// µÃµ½×ÜĞĞÊı
+			// å¾—åˆ°æ€»è¡Œæ•°
 			int rowNum = sheet.getLastRowNum();
 			Map<Integer, String> temp = new HashMap<Integer, String>();
 			readTitle(sheet, temp);
@@ -64,7 +64,7 @@ public class ReadUserXlsUtils  {
 			BeanInfo beanInfo = Introspector.getBeanInfo(UserBean.class);
 			List<String[]> records = new ArrayList<String[]>();
 			for (int i = 1; i <= rowNum; i++) {
-				// µ±Ç°ĞĞ
+				// å½“å‰è¡Œ
 				HSSFRow row = sheet.getRow(i);
 				int colNum = row.getLastCellNum();
 				// String[] data = new String[colNum];
