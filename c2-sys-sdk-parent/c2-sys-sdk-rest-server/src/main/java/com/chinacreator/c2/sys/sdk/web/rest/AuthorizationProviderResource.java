d4c03ac692446b51055a9d6d85dfbe37052fe9ae
严艺琳 <yilin.yan@chinacreator.com>
2016-12-21 16:29:45 +0800

    微调

---
+++ c2-sys-sdk-parent/c2-sys-sdk-rest-server/src/main/java/com/chinacreator/c2/sys/sdk/web/rest/AuthorizationProviderResource.java
@@ -0,0 +1,24 @@
+package com.chinacreator.c2.sys.sdk.web.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Path("/v1/remote/permissions")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Api("系统管理SDK - 统一鉴权接口")
+@ApiResponses({
+		@ApiResponse(code = 401, message = "用户没有登录", response = Error.class),
+		@ApiResponse(code = 403, message = "没有权限访问", response = Error.class), })
+public class AuthorizationProviderResource {
+
+}
