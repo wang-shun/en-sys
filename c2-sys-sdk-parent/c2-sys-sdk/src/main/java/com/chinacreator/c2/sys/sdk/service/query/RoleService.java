4f3c4083a88671f4c5561115c33b5e6a934495da
严艺琳 <yilin.yan@chinacreator.com>
2016-12-22 14:43:22 +0800

    添加角色信息查询接口

--- c2-sys-sdk-parent/c2-sys-sdk/src/main/java/com/chinacreator/c2/sys/sdk/service/query/RoleService.java
+++ c2-sys-sdk-parent/c2-sys-sdk/src/main/java/com/chinacreator/c2/sys/sdk/service/query/RoleService.java
@@ -1,20 +1,24 @@
-//TODO 暂不提供单独的角色查询接口
-//package com.chinacreator.c2.sys.sdk.service.query;
-//
-//import java.util.List;
-//
-//import com.chinacreator.c2.sys.sdk.bean.Role;
-//
-//
-///**
-// * 角色查询接口
-// * @author 彭盛
-// */
-//public interface RoleService {
-//    /**
-//     * 查询角色列表
-//     * @param role 查询条件对象，null表示查所有的
-//     * @return 角色列表
-//     */
-//    public List<Role> query(Role role);
-//}
+package com.chinacreator.c2.sys.sdk.service.query;
+
+import com.chinacreator.c2.sys.sdk.bean.Role;
+
+
+/**
+ * 角色查询接口
+ * @author 彭盛
+ */
+public interface RoleService {
+    /**
+     * 查询角色列表
+     * @param role 查询条件对象，null表示查所有的
+     * @return 角色列表
+     */
+    public Role get(String id);
+    
+    /**
+     * 查询角色列表
+     * @param role 查询条件对象，null表示查所有的
+     * @return 角色列表
+     */
+    public Role getByName(String roleName);
+}
