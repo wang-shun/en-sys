bc98b119e055335a45018dfd55013f4fef291d85
qiao.li <qiao.li@chiancreator.com>
2018-01-25 16:43:54 +0800

    improve

--- c2-sys-ui/src/main/java/cn/vvvv/vv/sys/access/AccessController.java
+++ c2-sys-ui/src/main/java/cn/vvvv/vv/sys/access/AccessController.java
@@ -3,23 +3,23 @@ package cn.vvvv.vv.sys.access;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
 import com.chinacreator.asp.comp.sys.core.security.service.AccessControlService;
 
-@RestController
+@Controller
 public class AccessController {
 
 	@Autowired
 	AccessControlService acs;
 	
+	@ResponseBody
 	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public Object loginAction(@RequestBody JSONObject params) {
 		String username = params.getString("username");
@@ -34,8 +34,18 @@ public class AccessController {
 		}
 	}
 	
+	@ResponseBody
 	@RequestMapping(value="logout")
-	public String logout(){
-		return acs.logout();
+	public Object logout(){
+		String url = acs.logout();
+		if(url != null) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("result", url);
+			return map;
+		} else {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("result", null);
+			return map;
+		}
 	}
 }
