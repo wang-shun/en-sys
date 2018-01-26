131d470b74bcea67d0221bb14b4cf1c50aec7f88
qiao.li <qiao.li@chiancreator.com>
2018-01-26 17:22:07 +0800

    restree content

---
+++ c2-sys-ui/src/main/java/cn/vvvv/vv/sysmgmt/sysset/resmgt/ResTreeContentProviderImpl.java
@@ -0,0 +1,76 @@
+package cn.vvvv.vv.sysmgmt.sysset.resmgt;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.chinacreator.asp.sysmgmt.sysset.resmgt.ResTreeNodeBuilder;
+import com.chinacreator.asp.sysmgmt.sysset.resmgt.ResourceTreeNode;
+import com.chinacreator.c2.web.ds.TreeContentProvider;
+import com.chinacreator.c2.web.ds.TreeContext;
+import com.chinacreator.c2.web.ds.TreeNode;
+
+@Service("restreecontent")
+public class ResTreeContentProviderImpl implements TreeContentProvider {
+
+	private static Map<String, List<ResourceTreeNode>> treeNodeMap;
+
+	@Override
+	public TreeNode[] getElements(TreeContext context) {
+		List<ResourceTreeNode> list = new ArrayList<ResourceTreeNode>();
+		if (null != context) {
+			Map<String, Object> map = context.getConditions();
+			String resId = (String) map.get("id");
+			String isLoad = (String) map.get("isLoad");
+
+			if (null == resId || resId.trim().equals("") || null == treeNodeMap || "true".equals(isLoad)) {
+				ResTreeNodeBuilder resBuilder = new ResTreeNodeBuilder();
+				treeNodeMap = resBuilder.build();
+			}
+
+			if (null == resId || resId.trim().equals("")) {
+				ResourceTreeNode rootTreeNode = new ResourceTreeNode(null, "0", "资源树", "root", true);
+				rootTreeNode.setChkDisabled(true);
+				rootTreeNode.setNocheck(true);
+				rootTreeNode.setNoteTitle("资源树");
+				rootTreeNode.setSn(0);
+				list.add(rootTreeNode);
+
+				resId = "0";
+			}
+
+			if (null != treeNodeMap && !treeNodeMap.isEmpty()) {
+				List<ResourceTreeNode> treeNodeList = treeNodeMap.get(resId);
+				if(null!=treeNodeList && !treeNodeList.isEmpty()){
+					list.addAll(treeNodeList);
+				}
+			}
+
+			
+		}
+
+		if (!list.isEmpty()) {
+			Collections.sort(list, new Comparator<ResourceTreeNode>() {
+				@Override
+				public int compare(ResourceTreeNode o1, ResourceTreeNode o2) {
+					Integer i1 = o1.getSn();
+					Integer i2 = o2.getSn();
+					if (null == i1) {
+						i1 = 0;
+					}
+					if (null == i2) {
+						i2 = 0;
+					}
+
+					return i1.compareTo(i2);
+				}
+			});
+		}
+
+		return list.toArray(new TreeNode[list.size()]);
+	}
+}
